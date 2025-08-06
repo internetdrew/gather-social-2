56acd90bf0743843f46155fa1492af26cacc5095
Andrew Rowley <andrew@internetdrew.com>
2025-08-06 15:40:33 -0400

    add theme toggling and new favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   return (
-    <div className="flex h-screen flex-col font-mono">
-      <Navbar />
-      <Outlet />
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex h-screen flex-col">
+        <Navbar />
+        <Outlet />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,29 @@
 import { Link } from "react-router";
+import { ThemeToggle } from "./theme-toggle";
 
 const Navbar = () => {
   return (
     <nav className="fixed flex w-full items-center justify-between p-4 text-center">
       <Link to="/" className="font-semibold">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-gallery-horizontal-end-icon lucide-gallery-horizontal-end mr-1.5 inline size-4 text-pink-600"
+        >
+          <path d="M2 7v10" />
+          <path d="M6 5v14" />
+          <rect width="12" height="18" x="10" y="3" rx="2" />
+        </svg>
         Gather Social
       </Link>
+      <ThemeToggle />
     </nav>
   );
 };
