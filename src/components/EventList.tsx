e0c28fbad3e652de16071c633323bac39f95bc59
Andrew Rowley <andrew@internetdrew.com>
2025-08-08 12:26:21 -0400

    display cards in a grid

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -9,7 +9,7 @@ const EventList = ({
   handleEventClick: (event: Tables<"events">) => void;
 }) => {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {events.map((event) => (
         <EventCard
           key={event.id}
