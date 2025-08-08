bbe5dd4ae7b67805314362896e0f1a0a2b56c947
Andrew Rowley <andrew@internetdrew.com>
2025-08-08 11:26:45 -0400

    add event list and possible event editing

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,36 @@
+import EventDialog from "@/components/dialogs/EventDialog";
 import NoEvents from "@/components/NoEvents";
+import { trpc } from "@/utils/trpc";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import type { Tables } from "../../shared/database.types";
+import EventList from "@/components/EventList";
 
 const Home = () => {
+  const [renderEventDialog, setRenderEventDialog] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState<Tables<"events"> | null>(
+    null,
+  );
+  const { data: events } = useQuery(trpc.event.list.queryOptions());
+
+  const handleEventClick = (event: Tables<"events">) => {
+    setSelectedEvent(event);
+    setRenderEventDialog(true);
+  };
+
   return (
     <div className="px-4">
-      <NoEvents />
+      {events && events.length > 0 ? (
+        <EventList events={events} handleEventClick={handleEventClick} />
+      ) : (
+        <NoEvents setRenderEventDialog={setRenderEventDialog} />
+      )}
+      <EventDialog
+        event={selectedEvent}
+        open={renderEventDialog}
+        onOpenChange={setRenderEventDialog}
+        setSelectedEvent={setSelectedEvent}
+      />
     </div>
   );
 };
