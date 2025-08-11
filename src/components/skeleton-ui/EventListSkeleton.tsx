bad7557d6c931c21f4d6788e75a2b6022e108593
Andrew Rowley <andrew@internetdrew.com>
2025-08-11 14:47:29 -0400

    add skeleton ui

---
+++ src/components/skeleton-ui/EventListSkeleton.tsx
@@ -0,0 +1,19 @@
+import { Skeleton } from "../ui/skeleton";
+
+const EventListSkeleton = () => {
+  return (
+    <>
+      <div className="mb-8 flex justify-between gap-2 px-4 sm:justify-end">
+        <Skeleton className="h-10 w-32 rounded-lg" />
+        <Skeleton className="h-10 w-32 rounded-lg" />
+      </div>
+      <div className="grid grid-cols-1 gap-4 px-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {Array.from({ length: 4 }).map((_, index) => (
+          <Skeleton key={index} className="h-36 w-full rounded-lg" />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default EventListSkeleton;
